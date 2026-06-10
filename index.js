38c9941a698d31804acadadc991a738632e331ec
farhad202526 <fa99.official@gmail.com>
2026-06-11 00:23:43 +0600

    add new commite

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const express = require('express');
 
 const cors = require ('cors');
 const { createRemoteJWKSet, jwtVerify } = require('jose-cjs');
+const dotenv = require('dotenv');
 dotenv.config();
 const app = express();
 
